3f1b232df087b36b371c0745b66c0823d4aa6f14
Vasya Novikov <n1dr+cm3513git@ya.ru>
2018-09-26 11:18:26 +0300

    WIP: added gnuplot tests

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -euET
 {
 
-export DISPLAY=":0"
-export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
+export DISPLAY="${DISPLAY:-:0}"
+export PATH='/home/vasya/bin:/home/vasya/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/lib/jvm/default/bin'
+export DBUS_SESSION_BUS_ADDRESS="${DBUS_SESSION_BUS_ADDRESS:-unix:path=/run/user/$(id -u)/bus}"
 
-"$(dirname -- "$(realpath -- "$0")")"/target/release/screenshooter-rs
+cd "$(dirname -- "$(realpath -- "$0")")"
+cargo run
+#"$(dirname -- "$(realpath -- "$0")")"/src/main.rs
+#exec "$(dirname -- "$(realpath -- "$0")")"/target/x86_64-unknown-linux-musl/release/screenshooter-rs
 
 exit
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #!/usr/bin/env run-cargo-script
+//cargo-deps: gnuplot="0.0.26"
 
 //use std::io::prelude::*;
 use std::process::Command;
-//use std::process::Stdio;
 //use std::{thread, time};
 
-//extern crate regex;
+extern crate gnuplot;
 
 fn main() {
 	println!("script launched, args: {:?}", std::env::args().skip(1).collect::<String>());
@@ -32,6 +32,28 @@ fn main() {
 	};
 	eprintln!("We're on desktop {:?} and our window is {:?}", desktop_number, window_name);
 
+	use gnuplot::*;
+	let x = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0];
+	let skip = [1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0];
+	let work = [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1];
+	let mut fg = Figure::new();
+	fg.set_terminal("svg", "/home/vasya/.cache/timeplot/timeplot.svg");
+	fg.axes2d()
+//		.set_x_axis(false, &[])
+//		.set_y_axis(false, &[Color("blue"), LineStyle(Dot)])
+		.set_x_ticks(None, &[], &[TextColor("blue")]) // Some((Auto, 0))
+		.set_y_ticks(None, &[], &[])
+		.set_border(false, &[], &[])
+//		.set_grid_options(false, &[LineStyle(Dot), Color("blue"), LineWidth(0.0)])
+//		.set_x_grid(false)
+//		.set_y_grid(false)
+//		.set_pos_grid(1, 1, 0)
+		.lines(&x, &skip, &[Caption(""), Color("orange"), PointSize(1.0), PointSymbol('*')])
+		.lines(&x, &work, &[Caption(""), Color("black"), PointSize(1.0), PointSymbol('*')])
+		.set_y_range(Fix(-0.1), Fix(10.1));
+	fg.echo_to_file("/home/vasya/.cache/timeplot/timeplot.gp");
+	fg.show();
+
 //	let name_contains_closure = |pattern| regex::Regex::new(pattern).unwrap().is_match(test);
 
 //	fn nameContains(pattern: &str) -> bool {
@@ -39,12 +61,4 @@ fn main() {
 //		return true
 //	}
 
-	//	let command = Command::new("xprintidle").output().unwrap();
-
-	//	let window_name = Command::new("xdotool")
-	//			.arg("getactivewindow").arg("getwindowname")
-	//			.output().unwrap().stdout;
-	//	let window_name = String::from_utf8_lossy(&window_name);
-	//	let window_name = window_name.trim();
-	//	println!("current window: {}", window_name);
 }
