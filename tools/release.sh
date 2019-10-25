5153e8b57532c5fd90b1d156341046ea3abbbb7e
Vasilii Novikov <n1dr+cm3513git@ya.ru>
2019-10-25 09:41:42 +0200

    update release script

--- tools/release.sh
+++ tools/release.sh
@@ -1,22 +1,25 @@
 #!/bin/bash -euET
 {
+# script to release the project, probably has no use to anybody else except for reference.
+
 set -o pipefail
 
+rustup update
 cargo upgrade
 cargo update
-
-# check code and update Cargo.lock:
-cargo check --target i686-pc-windows-gnu
-cargo check --target i686-unknown-linux-musl
-cargo check --target i686-apple-darwin
-
+cargo fmt --all --
 if ! test -z "$(git status --porcelain)"; then
-	>&2 printf '%s\n' "error: uncommitted changes"
-	exit 1
+  >&2 printf '%s\n' "error: uncommitted changes"
+  exit 1
 fi
 
+cargo test
+cargo clippy --all-targets --all-features -- -D warnings
+cargo check --target i686-pc-windows-gnu
+cargo check --target i686-apple-darwin
 cargo build --release
 cargo build --target=i686-unknown-linux-musl --release
+
 upx --ultra-brute target/i686-unknown-linux-musl/release/timeplot
 cp target/i686-unknown-linux-musl/release/timeplot .vasya-personal/tpl/
 
